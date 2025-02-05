3cd88d89c6546f7822c5c4f5cb1e5b700fb20922
FaithNnakwe <ujunwannakwe@gmail.com>
2025-02-05 15:45:52 -0500

    Basic UI with input field and button.

--- lib/main.dart
+++ lib/main.dart
@@ -20,8 +20,15 @@ class WeatherInfoApp extends StatelessWidget {
   }
 }
 
-class WeatherHomePage extends StatelessWidget {
+class WeatherHomePage extends StatefulWidget {
   const WeatherHomePage({super.key});
+
+  @override
+  State<WeatherHomePage> createState() => _WeatherHomePageState();
+}
+
+class _WeatherHomePageState extends State<WeatherHomePage> {
+  final TextEditingController _cityController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -29,8 +36,28 @@ class WeatherHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Weather Info App'),
       ),
-      body: const Center(
-        child: Text('Welcome to the Weather Info App!'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _cityController,
+              decoration: const InputDecoration(
+                labelText: 'Enter City Name', // Activity 2.a: Button to input the City Name.
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Fetch Weather'),  // Activity 2.b: Add a FlatButton or ElevatedButton labeled "Fetch Weather".
+            ),
+            const SizedBox(height: 16),
+            const Text('City: '),   // Activity2.c: Add placeholder for diplaying weather data like: city.
+            const Text('Temperature: '), // Tempeture.
+            const Text('Condition: '), // Weather condition.
+          ],
+        ),
       ),
     );
   }
